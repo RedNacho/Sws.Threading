75e7c29ea3998e7bff011c3996098607354217c3
RedNachoBitbucket <rednachobitbucket@example.com>
2014-08-17 17:59:35 +0100

    Added the ability to limit member expressions to getters or setters.  Also added more complex example code to the Demo project illustrating some of the more advanced ways of proxying.

---
+++ Sws.Threading.Demo/ReaderWriterLockExample.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Sws.Threading.Extensions;
+
+namespace Sws.Threading.Demo
+{
+    public class ReadLock : ILock
+    {
+        private readonly ReaderWriterLockSlim _readerWriterLockSlim;
+
+        public ReadLock(ReaderWriterLockSlim readerWriterLockSlim)
+        {
+            if (readerWriterLockSlim == null)
+            {
+                throw new ArgumentNullException("readerWriterLockSlim");
+            }
+
+            _readerWriterLockSlim = readerWriterLockSlim;
+        }
+
+        public void Enter()
+        {
+            _readerWriterLockSlim.EnterReadLock();
+        }
+
+        public void Exit()
+        {
+            _readerWriterLockSlim.ExitReadLock();
+        }
+
+    }
+
+    public class WriteLock : ILock
+    {
+        private readonly ReaderWriterLockSlim _readerWriterLockSlim;
+
+        public WriteLock(ReaderWriterLockSlim readerWriterLockSlim)
+        {
+            if (readerWriterLockSlim == null)
+            {
+                throw new ArgumentNullException("readerWriterLockSlim");
+            }
+
+            _readerWriterLockSlim = readerWriterLockSlim;
+        }
+
+        public void Enter()
+        {
+            _readerWriterLockSlim.EnterWriteLock();
+        }
+
+        public void Exit()
+        {
+            _readerWriterLockSlim.ExitWriteLock();
+        }
+
+    }
+
+    public class Container
+    {
+        public virtual int Value
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            var container = new Container();
+
+            var readerWriterLockSlim = new ReaderWriterLockSlim();
+
+            // The proxied container will apply the semantics of a ReaderWriterLock to the Value property.
+
+            container = container
+                .ConfigureThreadSafeProxy().ForGetter(obj => obj.Value)
+                .WithLockingObject(readerWriterLockSlim).WithLockFactory(lockingObject => new ReadLock(lockingObject as ReaderWriterLockSlim)).Build()
+                .ConfigureThreadSafeProxy().ForSetter(obj => obj.Value)
+                .WithLockingObject(readerWriterLockSlim).WithLockFactory(lockingObject => new WriteLock(lockingObject as ReaderWriterLockSlim)).Build();
+        }
+    }
+
+}
