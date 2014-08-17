0074b25955461b99f417a67710b92159aefa1e84
RedNachoBitbucket <rednachobitbucket@example.com>
2014-08-17 13:00:02 +0100

    Made a start on triple-slash docs.

--- Sws.Threading/Extensions/ObjectExtensions.cs
+++ Sws.Threading/Extensions/ObjectExtensions.cs
@@ -10,10 +10,25 @@ namespace Sws.Threading.Extensions
     public static class ObjectExtensions
     {
 
+        /// <summary>
+        /// Creates a thread-safe proxy of the supplied object, similar to adding "synchronized" to all members in Java.
+        /// </summary>
+        /// <typeparam name="TProxy"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+
         public static TProxy ThreadSafeProxy<TProxy>(this TProxy obj) where TProxy : class
         {
             return ConfigureThreadSafeProxy(obj).Build();
         }
+
+        /// <summary>
+        /// Creates a thread-safe proxy builder which you can use to configure a thread-safe proxy for the object.
+        /// Call Build() when finished.
+        /// </summary>
+        /// <typeparam name="TProxy"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
 
         public static ThreadSafeProxyBuilder<TProxy> ConfigureThreadSafeProxy<TProxy>(this TProxy obj) where TProxy : class
         {
--- Sws.Threading/ILock.cs
+++ Sws.Threading/ILock.cs
@@ -5,9 +5,20 @@ using System.Text;
 
 namespace Sws.Threading
 {
+
+    /// <summary>
+    /// Abstraction for a thread lock (e.g. a Mutex).
+    /// </summary>
     public interface ILock
     {
+        /// <summary>
+        /// Enters the lock.
+        /// </summary>
         void Enter();
+
+        /// <summary>
+        /// Exits the lock.
+        /// </summary>
         void Exit();
     }
 }
