5a8f14cfbe346029ac8aece13a668033d50179f6
RedNachoBitbucket <rednachobitbucket@example.com>
2014-08-23 12:45:07 +0100

    Added brief example code using Nindapter (https://github.com/RedNacho/Sws.Nindapter) to generate a thread-safe proxy.

---
+++ Sws.Threading.Nindapter.Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+using Sws.Threading.Extensions;
+using Sws.Nindapter.Extensions;
+
+namespace Sws.Threading.Ninject
+{
+
+    public interface IContainer
+    {
+        int Value { get; set; }
+    }
+
+    public class UnsafeContainer : IContainer
+    {
+        public int Value { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind<IContainer>().ThroughDecorator(c => c.ThreadSafeProxy()).To<UnsafeContainer>();
+
+            var container = kernel.Get<IContainer>();
+        }
+    }
+}
