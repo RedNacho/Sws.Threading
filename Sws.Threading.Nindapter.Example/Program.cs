4768ebcfc3ccdb9147622c2eaa6c6c2935d72022
RedNacho <simon_bluescandal@yahoo.co.uk>
2014-08-24 18:01:33 +0100

    Fleshed out Nindapter example a little.

--- Sws.Threading.Nindapter.Example/Program.cs
+++ Sws.Threading.Nindapter.Example/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Ninject;
 using Sws.Threading.Extensions;
 using Sws.Nindapter.Extensions;
@@ -11,12 +12,37 @@ namespace Sws.Threading.Ninject
 
     public interface IContainer
     {
-        int Value { get; set; }
+        int Value1 { get; }
+        int Value2 { get; }
+        void UpdateValues();
     }
 
     public class UnsafeContainer : IContainer
     {
-        public int Value { get; set; }
+        public int Value1
+        {
+            get;
+            private set;
+        }
+
+        public int Value2
+        {
+            get;
+            private set;
+        }
+
+        public void UpdateValues()
+        {
+            if (Value1 != Value2)
+            {
+                throw new Exception("Values don't match!  This method is not thread-safe!");
+            }
+
+            var value = Value1 + 1;
+
+            this.Value1 = value;
+            this.Value2 = value;
+        }
     }
 
     class Program
@@ -25,9 +51,29 @@ namespace Sws.Threading.Ninject
         {
             var kernel = new StandardKernel();
 
-            kernel.Bind<IContainer>().ThroughDecorator(c => c.ThreadSafeProxy()).To<UnsafeContainer>();
+            kernel.Bind<IContainer>().To<UnsafeContainer>().Named("Unsafe");
 
-            var container = kernel.Get<IContainer>();
+            kernel.Bind<IContainer>().ThroughDecorator(
+                c => c.ConfigureThreadSafeProxy()
+                    .ForMember(con => con.UpdateValues())
+                    .Build()
+            ).To<UnsafeContainer>().Named("Safe");
+
+            var container = kernel.Get<IContainer>("Safe"); // Swap this for the unsafe one to see the problem.
+
+            var usedVals = new List<int>();
+            
+            Parallel.Invoke(Enumerable.Range(0, 10).Select(value => (Action)(() => {
+
+                for (int iteration = 0; iteration < 10000; iteration++)
+                {
+                    container.UpdateValues();
+                }
+            })).ToArray());
+
+            Console.WriteLine("Everything OK.");
+
+            Console.ReadKey();
         }
     }
 }
